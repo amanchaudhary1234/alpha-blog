b0640d18145355b3b707ebeb120f5ce062eb3b5d
Aman <amanchaudhary4437@gmail.com>
2021-06-25 11:58:52 +0530

    my first project

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,18 @@
+class ArticlesController < ApplicationController
+    def show
+    @article = Article.find(params[:id])
+    end
+    def index
+        @article = Article.all
+    end
+    def new
+
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+       if @article.save
+       redirect_to @article
+       else 
+        render 'new'
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   get "/articles", to: "articles#index"
-end
+root 'pages#home' 
+get 'about', to: 'pages#about'
+resources :articles#, only: [:show, :index,  :new, :create]
+ end
