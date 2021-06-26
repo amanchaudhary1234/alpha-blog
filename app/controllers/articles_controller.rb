2921e6d94c152bdb41202f4b045051cf2a231698
Aman <amanchaudhary4437@gmail.com>
2021-06-26 09:10:08 +0530

    CRUD operation perform on a project

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,13 +6,28 @@ class ArticlesController < ApplicationController
         @article = Article.all
     end
     def new
-
+  @article = Article.new
     end
+  def edit
+    @article = Article.find(params[:id])
+ end
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
        if @article.save
+        flash[:notice] = "Articles was created successfully ."
        redirect_to @article
        else 
         render 'new'
     end
+end
+
+def update
+    @article = Article.find(params[:id])
+   if @article.update(params.require(:article).permit(:title, :description))
+   flash[:notice] = "Articles was updated successfully ."
+   redirect_to @article
+   else
+    render 'edit'
+  end
+end
 end
