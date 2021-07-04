8b15e0891206234cb44a1995e9d93bb066c23942
Aman <amanchaudhary4437@gmail.com>
2021-07-04 15:33:38 +0530

    initial commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,33 +1,45 @@
 class ArticlesController < ApplicationController
-    def show
+
+  def show
     @article = Article.find(params[:id])
-    end
-    def index
-        @article = Article.all
-    end
-    def new
-  @article = Article.new
-    end
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
   def edit
     @article = Article.find(params[:id])
- end
-    def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
-       if @article.save
-        flash[:notice] = "Articles was created successfully ."
-       redirect_to @article
-       else 
-        render 'new'
-    end
-end
+  end
 
-def update
+  def create
+     @article = Article.new(params.require(:article).permit(:title, :description))
+     if @article.save
+       flash[:notice] = "Articles was created successfully ."
+       redirect_to @article
+     else 
+       render 'new'
+     end
+  end
+
+  def update 
     @article = Article.find(params[:id])
-   if @article.update(params.require(:article).permit(:title, :description))
-   flash[:notice] = "Articles was updated successfully ."
-   redirect_to @article
-   else
-    render 'edit'
+    if @article.update(params.require(:article).permit(:title, :description))
+  	  flash[:notice] = "Articles was updated successfully ."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+		
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
 end
